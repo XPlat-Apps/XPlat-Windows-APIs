e35f0f367e05fd7279e920556d44c236998fa675
James Croft <jamz_c@hotmail.co.uk>
2016-06-28 13:19:56 +0100

    Removed sample apps and replaced with Unit Test applications.

---
+++ XamKit/XamKit.Testing.UWP/UnitTest.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
+
+namespace XamKit.Testing.UWP
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
