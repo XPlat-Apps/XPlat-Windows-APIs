4902d86d3475c4573b9ab45b5c3e4d4ffd73e842
James Croft <jamz_c@hotmail.co.uk>
2016-06-29 08:17:48 +0100

    Added more test coverage for UWP storage

--- XamKit/XamKit.Testing.UWP/Tests/Core/Tests.Storage.cs
+++ XamKit/XamKit.Testing.UWP/Tests/Core/Tests.Storage.cs
@@ -1,9 +1,13 @@
-﻿namespace XamKit.Testing.UWP
+﻿namespace XamKit.Testing.UWP.Tests.Core
 {
+    using System;
+    using System.Threading.Tasks;
+
     using Microsoft.VisualStudio.TestPlatform.UnitTestFramework;
 
     using XamKit.Core.Common.Storage;
     using XamKit.Core.Storage;
+    using XamKit.Testing.UWP.UnitTestFramework;
 
     [TestClass]
     public partial class Tests
@@ -15,25 +19,73 @@
         }
 
         [TestMethod]
-        public void ApplicationStorage_CurrentLocalFolder_IsNotNull()
+        public void ApplicationStorage_Current_LocalFolder_IsNotNull()
         {
             Assert.IsNotNull(ApplicationStorage.Current.LocalFolder);
         }
 
         [TestMethod]
-        public void ApplicationStorage_CurrentRoamingFolder_IsNotNull()
+        public void ApplicationStorage_Current_RoamingFolder_IsNotNull()
         {
             Assert.IsNotNull(ApplicationStorage.Current.RoamingFolder);
         }
 
         [TestMethod]
-        public async void ApplicationStorage_CurrentLocalFolder_CreateFileAsync_Exists()
+        public async Task ApplicationStorage_Current_LocalFolder_CreatedFileExists()
         {
-            var file = await ApplicationStorage.Current.LocalFolder.CreateFileAsync(
-                "test.txt",
-                FileStoreCreationOption.ReplaceIfExists);
+            var file =
+                await
+                ApplicationStorage.Current.LocalFolder.CreateFileAsync(
+                    "test.txt",
+                    FileStoreCreationOption.ReplaceIfExists);
 
             Assert.IsTrue(file.Exists);
+        }
+
+        [TestMethod]
+        public async Task ApplicationStorage_Current_LocalFolder_NotSupportedExceptionThrownWhenCreatingFileThatExists()
+        {
+            const string FileName = "test.txt";
+
+            await
+                ApplicationStorage.Current.LocalFolder.CreateFileAsync(
+                    FileName,
+                    FileStoreCreationOption.ReplaceIfExists);
+
+            await
+                AssertAsync.ThrowsExceptionAsync<Exception>(
+                    async () => await ApplicationStorage.Current.LocalFolder.CreateFileAsync(FileName));
+        }
+
+        [TestMethod]
+        public async Task ApplicationStorage_Current_LocalFolder_CreatedFileIsDeleted()
+        {
+            var file =
+                await
+                ApplicationStorage.Current.LocalFolder.CreateFileAsync(
+                    "test.txt",
+                    FileStoreCreationOption.ReplaceIfExists);
+
+            await file.DeleteAsync();
+
+            Assert.IsFalse(file.Exists);
+        }
+
+        [TestMethod]
+        public async Task ApplicationStorage_Current_LocalFolder_NotSupportedExceptionThrownWhenWritingTextToDeletedFile
+            ()
+        {
+            var file =
+                await
+                ApplicationStorage.Current.LocalFolder.CreateFileAsync(
+                    "test.txt",
+                    FileStoreCreationOption.ReplaceIfExists);
+
+            await file.DeleteAsync();
+
+            await
+                AssertAsync.ThrowsExceptionAsync<NotSupportedException>(
+                    async () => await file.WriteTextAsync("Hello, World"));
         }
     }
 }
