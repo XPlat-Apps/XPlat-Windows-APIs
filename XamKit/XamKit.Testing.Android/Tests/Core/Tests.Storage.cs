6c4ecfdd9b9c2ffbd802db1db69cc75844c4c5a6
James Croft <jamz_c@hotmail.co.uk>
2016-07-04 13:38:00 +0100

    Re-added Android unit tests

--- XamKit/XamKit.Testing.Android/Tests/Core/Tests.Storage.cs
+++ XamKit/XamKit.Testing.Android/Tests/Core/Tests.Storage.cs
@@ -2,31 +2,37 @@ namespace XamKit.Testing.Android.Tests.Core
 {
     using NUnit.Framework;
 
+    using XamKit.Core.Storage;
+
     [TestFixture]
     public partial class Tests
     {
         [SetUp]
-        public void Setup() { }
+        public void Setup()
+        {
+        }
 
         [TearDown]
-        public void Tear() { }
+        public void Tear()
+        {
+        }
 
-        //[Test]
-        //public void ApplicationStorage_CanAccessCurrent()
-        //{
-        //    Assert.IsNotNull(ApplicationStorage.Current);
-        //}
+        [Test]
+        public void ApplicationStorage_CanAccessCurrent()
+        {
+            Assert.IsNotNull(ApplicationStorage.Current);
+        }
 
-        //[Test]
-        //public void ApplicationStorage_CanAccessLocalFolder()
-        //{
-        //    Assert.IsNotNull(ApplicationStorage.Current.LocalFolder);
-        //}
+        [Test]
+        public void ApplicationStorage_CanAccessLocalFolder()
+        {
+            Assert.IsNotNull(ApplicationStorage.Current.LocalFolder);
+        }
 
-        //[Test]
-        //public void ApplicationStorage_CannotAccessRoamingFolder()
-        //{
-        //    Assert.IsNull(ApplicationStorage.Current.RoamingFolder);
-        //}
+        [Test]
+        public void ApplicationStorage_CannotAccessRoamingFolder()
+        {
+            Assert.IsNull(ApplicationStorage.Current.RoamingFolder);
+        }
     }
 }
