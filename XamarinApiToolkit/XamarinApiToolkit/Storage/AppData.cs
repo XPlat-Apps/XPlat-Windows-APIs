42fa363c2209f19ecfc1f150e793c82e3e9e6e09
James Croft <jamz_c@hotmail.co.uk>
2016-10-25 15:42:02 +0100

    Updated AppData and linked to other projects.

--- XamarinApiToolkit/XamarinApiToolkit/Storage/AppData.cs
+++ XamarinApiToolkit/XamarinApiToolkit/Storage/AppData.cs
@@ -1,15 +1,112 @@
 ﻿namespace XamarinApiToolkit.Storage
 {
+    using System;
+    using System.Threading;
+
+    /// <summary>
+    /// Defines the application data layer.
+    /// </summary>
     public sealed class AppData : IAppData
     {
-        public IAppFolder LocalFolder { get; }
+        private static readonly Lazy<AppData> CurrentAppData = new Lazy<AppData>(
+                                                                   () => new AppData(),
+                                                                   LazyThreadSafetyMode.PublicationOnly);
 
-        public IAppSettingsContainer LocalSettings { get; }
+        private readonly Lazy<IAppSettingsContainer> settings = new Lazy<IAppSettingsContainer>(
+                                                                    CreateSettings,
+                                                                    LazyThreadSafetyMode.PublicationOnly);
 
-        public IAppFolder RoamingFolder { get; }
+        private readonly Lazy<IAppFolder> localFolder = new Lazy<IAppFolder>(
+                                                            CreateLocalFolder,
+                                                            LazyThreadSafetyMode.PublicationOnly);
 
-        public IAppFolder TemporaryFolder { get; }
 
-        public static AppData Current { get; }
+
+        private readonly Lazy<IAppFolder> roamingFolder = new Lazy<IAppFolder>(
+                                                              CreateRoamingFolder,
+                                                              LazyThreadSafetyMode.PublicationOnly);
+
+        private readonly Lazy<IAppFolder> temporaryFolder = new Lazy<IAppFolder>(
+                                                                CreateTemporaryFolder,
+                                                                LazyThreadSafetyMode.PublicationOnly);
+
+        /// <summary>
+        /// Gets the current instance of the <see cref="AppData"/>.
+        /// </summary>
+        public static AppData Current
+        {
+            get
+            {
+                return CurrentAppData.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the root folder for the application in the local data store.
+        /// </summary>
+        public IAppFolder LocalFolder
+        {
+            get
+            {
+                return this.localFolder.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the settings container for the application in the local data store.
+        /// </summary>
+        public IAppSettingsContainer LocalSettings
+        {
+            get
+            {
+                return this.settings.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the root folder for the application in the roaming data store.
+        /// </summary>
+        public IAppFolder RoamingFolder
+        {
+            get
+            {
+                return this.roamingFolder.Value;
+            }
+        }
+
+        /// <summary>
+        /// Gets the root folder for the application in the temporary data store.
+        /// </summary>
+        public IAppFolder TemporaryFolder
+        {
+            get
+            {
+                return this.temporaryFolder.Value;
+            }
+        }
+
+        private static IAppSettingsContainer CreateSettings()
+        {
+            // ToDo
+            return null;
+        }
+
+        private static IAppFolder CreateLocalFolder()
+        {
+            // ToDo
+            return null;
+        }
+
+        private static IAppFolder CreateRoamingFolder()
+        {
+            // ToDo
+            return null;
+        }
+
+        private static IAppFolder CreateTemporaryFolder()
+        {
+            // ToDo
+            return null;
+        }
     }
 }
