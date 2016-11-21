60f9e471c0bb56d3cbcb9de88731e1b7492e4696
James Croft <jamz_c@hotmail.co.uk>
2016-11-21 19:06:02 +0000

    Removed test project, added an app project and unit test project using Appium for UI automation.

---
+++ XamarinApiToolkit/XamarinApiToolkit.UITesting/UWP/Tests/MessageDialog_Tests.cs
@@ -0,0 +1,39 @@
+﻿namespace XamarinApiToolkit.UITesting.UWP.Tests
+{
+    using System;
+
+    using System.Globalization;
+    using System.Threading;
+
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    using OpenQA.Selenium.Appium;
+
+    using OpenQA.Selenium.Appium.iOS; // Temporary placeholder until Windows namespace exists
+
+    using OpenQA.Selenium.Remote;
+
+    [TestClass]
+    public class MessageDialog_Tests
+    {
+        [ClassInitialize]
+        public static void Setup(TestContext context)
+        {
+            UWPAppHelper.Initialize();
+        }
+
+        [ClassCleanup]
+        public static void TearDown()
+        {
+            UWPAppHelper.Cleanup();
+        }
+
+        [TestMethod]
+        public void AppLaunch_Test()
+        {
+            Thread.Sleep(6000);
+
+            Assert.IsTrue(true);
+        }
+    }
+}
