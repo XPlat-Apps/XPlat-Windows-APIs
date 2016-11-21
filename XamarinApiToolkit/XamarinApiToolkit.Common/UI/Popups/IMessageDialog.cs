3ad6a13c508ecb6d7fee5e6db4351dd61b47f4e0
James Croft <jamz_c@hotmail.co.uk>
2016-11-21 16:47:34 +0000

    Added tests solution.
    Started adding MessageDialog API

---
+++ XamarinApiToolkit/XamarinApiToolkit.Common/UI/Popups/IMessageDialog.cs
@@ -0,0 +1,16 @@
+﻿namespace XamarinApiToolkit.UI.Popups
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+
+    public interface IMessageDialog
+    {
+        string Title { get; set; }
+
+        string Content { get; set; }
+
+        List<DialogCommand> Commands { get; }
+
+        Task ShowAsync();
+    }
+}
