aa81badb403a3db89774bd62cbdac653fa40ab04
James Croft <jamz_c@hotmail.co.uk>
2017-04-03 14:24:03 +0100

    Restructured Device.Display APIs

---
+++ XPlat.Device.Display.iOS/DisplayRequest.cs
@@ -0,0 +1,19 @@
+﻿namespace XPlat.Device.Display
+{
+    using UIKit;
+
+    public class DisplayRequest : IDisplayRequest
+    {
+        /// <inheritdoc />
+        public void RequestActive()
+        {
+            UIApplication.SharedApplication.IdleTimerDisabled = true;
+        }
+
+        /// <inheritdoc />
+        public void RequestRelease()
+        {
+            UIApplication.SharedApplication.IdleTimerDisabled = false;
+        }
+    }
+}
---
+++ XPlat.Device.Display/DisplayRequest.cs
@@ -0,0 +1,27 @@
+﻿namespace XPlat.Device.Display
+{
+    public class DisplayRequest : IDisplayRequest
+    {
+        private readonly Windows.System.Display.DisplayRequest request;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DisplayRequest"/> class.
+        /// </summary>
+        public DisplayRequest()
+        {
+            this.request = new Windows.System.Display.DisplayRequest();
+        }
+
+        /// <inheritdoc />
+        public void RequestActive()
+        {
+            this.request?.RequestActive();
+        }
+
+        /// <inheritdoc />
+        public void RequestRelease()
+        {
+            this.request?.RequestRelease();
+        }
+    }
+}
