8bfb5ee647548be88cf7fc08d2ee2c57f5973265
James Croft <jamz_c@hotmail.co.uk>
2017-04-03 22:30:33 +0100

    Added testing apps.

---
+++ XPlat.NuGet.Droid/MainActivity.cs
@@ -0,0 +1,86 @@
+﻿namespace XPlat.NuGet.Droid
+{
+    using System;
+
+    using Android.App;
+    using Android.OS;
+
+    using XPlat.Device.Display;
+    using XPlat.Device.Geolocation;
+    using XPlat.Storage;
+    using XPlat.Storage.Pickers;
+
+    [Activity(Label = "XPlat.NuGet.Droid", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private Geolocator geolocator;
+
+        protected override async void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            var request = new DisplayRequest(this.Window);
+
+            request.RequestActive();
+
+            var file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(
+                           "HelloWorld.txt",
+                           CreationCollisionOption.OpenIfExists);
+
+            await file.WriteTextAsync("Hello from the Android app!");
+
+            var batteryStatus = Device.Power.PowerManager.Current.BatteryStatus;
+            var remainingPercentage = Device.Power.PowerManager.Current.RemainingChargePercent;
+
+            Device.Power.PowerManager.Current.BatteryStatusChanged += this.PowerManager_BatteryStatusChanged;
+            Device.Power.PowerManager.Current.RemainingChargePercentChanged +=
+                this.PowerManager_RemainingChargePercentChanged;
+
+            this.geolocator = new Geolocator(this)
+                                  {
+                                      DesiredAccuracy = PositionAccuracy.Default,
+                                      MovementThreshold = 25
+                                  };
+            this.geolocator.PositionChanged += this.Geolocator_PositionChanged;
+
+            var access = await this.geolocator.RequestAccessAsync();
+            if (access == GeolocationAccessStatus.Allowed)
+            {
+                var currentLocation = await this.geolocator.GetGeopositionAsync(
+                                          new TimeSpan(0, 0, 10),
+                                          new TimeSpan(0, 0, 5));
+                if (currentLocation != null)
+                {
+                    // ToDo
+                }
+            }
+
+            var singleFilePick = new FileOpenPicker(this);
+            singleFilePick.FileTypeFilter.Add(".jpg");
+            var pickedFile = await singleFilePick.PickSingleFileAsync();
+
+            var multiFilePick = new FileOpenPicker(this);
+            multiFilePick.FileTypeFilter.Add(".jpg");
+            var pickedFiles = await multiFilePick.PickMultipleFilesAsync();
+
+            var fileData = await file.ReadTextAsync();
+
+            request.RequestRelease();
+        }
+
+        private void PowerManager_RemainingChargePercentChanged(object sender, int i)
+        {
+            var percent = i;
+        }
+
+        private void PowerManager_BatteryStatusChanged(object sender, Device.Power.BatteryStatus batteryStatus)
+        {
+            var status = batteryStatus;
+        }
+
+        private void Geolocator_PositionChanged(IGeolocator sender, PositionChangedEventArgs args)
+        {
+            var position = args.Position;
+        }
+    }
+}
---
+++ XPlat.NuGet.UWP/MainPage.xaml.cs
@@ -0,0 +1,88 @@
+﻿namespace XPlat.NuGet.UWP
+{
+    using System;
+
+    using Windows.UI.Xaml.Controls;
+    using Windows.UI.Xaml.Navigation;
+
+    using XPlat.Device.Display;
+    using XPlat.Device.Geolocation;
+    using XPlat.Device.Power;
+    using XPlat.Storage;
+    using XPlat.Storage.Pickers;
+
+    public sealed partial class MainPage : Page
+    {
+        private Geolocator geolocator;
+
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+
+        protected override async void OnNavigatedTo(NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            var request = new DisplayRequest();
+            request.RequestActive();
+
+            var file = await ApplicationData.Current.TemporaryFolder.CreateFileAsync(
+                           "HelloWorld.txt",
+                           CreationCollisionOption.OpenIfExists);
+
+            await file.WriteTextAsync("Hello from the Android app!");
+
+            var batteryStatus = Device.Power.PowerManager.Current.BatteryStatus;
+            var remainingPercentage = Device.Power.PowerManager.Current.RemainingChargePercent;
+
+            Device.Power.PowerManager.Current.BatteryStatusChanged += this.PowerManager_BatteryStatusChanged;
+            Device.Power.PowerManager.Current.RemainingChargePercentChanged +=
+                this.PowerManager_RemainingChargePercentChanged;
+
+            this.geolocator = new Geolocator { DesiredAccuracy = PositionAccuracy.Default, MovementThreshold = 25 };
+            this.geolocator.PositionChanged += this.Geolocator_PositionChanged;
+            var access = await this.geolocator.RequestAccessAsync();
+
+            if (access == GeolocationAccessStatus.Allowed)
+            {
+                var currentLocation = await this.geolocator.GetGeopositionAsync(
+                                          new TimeSpan(0, 0, 10),
+                                          new TimeSpan(0, 0, 5));
+
+                if (currentLocation != null)
+                {
+                    // ToDo
+                }
+            }
+
+            var singleFilePick = new FileOpenPicker();
+            singleFilePick.FileTypeFilter.Add(".jpg");
+            var pickedFile = await singleFilePick.PickSingleFileAsync();
+
+
+            var multiFilePick = new FileOpenPicker();
+            multiFilePick.FileTypeFilter.Add(".jpg");
+            var pickedFiles = await multiFilePick.PickMultipleFilesAsync();
+
+            var fileData = await file.ReadTextAsync();
+
+            request.RequestRelease();
+        }
+
+        private void PowerManager_RemainingChargePercentChanged(object sender, int e)
+        {
+            var percent = e;
+        }
+
+        private void PowerManager_BatteryStatusChanged(object sender, BatteryStatus e)
+        {
+            var status = e;
+        }
+
+        private void Geolocator_PositionChanged(IGeolocator sender, PositionChangedEventArgs args)
+        {
+            var position = args.Position;
+        }
+    }
+}
