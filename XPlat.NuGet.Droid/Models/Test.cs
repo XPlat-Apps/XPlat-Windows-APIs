b264118678db34ec1cba9e2df06fec16fa9647f8
James Croft <jamz_c@hotmail.co.uk>
2017-05-19 16:30:44 +0100

    Updated NuGet test area to confirm changes.

---
+++ XPlat.NuGet.Droid/Models/Test.cs
@@ -0,0 +1,13 @@
+﻿namespace XPlat.NuGet.Droid.Models
+{
+    using System;
+
+    public class Test
+    {
+        public string Name { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public Test NestedTest { get; set; }
+    }
+}
