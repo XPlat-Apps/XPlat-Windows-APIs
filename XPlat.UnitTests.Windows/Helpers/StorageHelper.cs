d383c3c8149137fc8df5c42da29d7bb1d9ad50d2
James Croft <jamz_c@hotmail.co.uk>
2018-09-16 19:29:25 +0100

    Added changes to StorageHelper class in unit test projects to better return exceptions when thrown.
    Added assets for banner and icon.

--- XPlat.UnitTests.Windows/Helpers/StorageHelper.cs
+++ XPlat.UnitTests.Windows/Helpers/StorageHelper.cs
@@ -10,15 +10,13 @@ namespace XPlat.UnitTests.Windows.Helpers
         public static IStorageFile CreateStorageFile(IStorageFolder folder, string name)
         {
             Task<IStorageFile> storageFileTask = folder.CreateFileAsync(name);
-            storageFileTask.Wait();
-            return storageFileTask.Result;
+            return storageFileTask.GetAwaiter().GetResult();
         }
 
         public static IStorageFolder CreateStorageFolder(IStorageFolder folder, string name)
         {
             Task<IStorageFolder> storageFileTask = folder.CreateFolderAsync(name);
-            storageFileTask.Wait();
-            return storageFileTask.Result;
+            return storageFileTask.GetAwaiter().GetResult();
         }
 
         public static IStorageFile CreateStorageFile(
@@ -27,8 +25,7 @@ namespace XPlat.UnitTests.Windows.Helpers
             CreationCollisionOption collisionOption)
         {
             Task<IStorageFile> storageFileTask = folder.CreateFileAsync(name, collisionOption);
-            storageFileTask.Wait();
-            return storageFileTask.Result;
+            return storageFileTask.GetAwaiter().GetResult();
         }
 
         public static IStorageFolder CreateStorageFolder(
@@ -37,48 +34,43 @@ namespace XPlat.UnitTests.Windows.Helpers
             CreationCollisionOption collisionOption)
         {
             Task<IStorageFolder> storageFileTask = folder.CreateFolderAsync(name, collisionOption);
-            storageFileTask.Wait();
-            return storageFileTask.Result;
+            return storageFileTask.GetAwaiter().GetResult();
         }
 
         public static IReadOnlyList<IStorageItem> GetFolderItems(IStorageFolder folder, int idx, int count)
         {
             Task<IReadOnlyList<IStorageItem>> itemsTask = folder.GetItemsAsync(idx, count);
-            itemsTask.Wait();
-            return itemsTask.Result;
+            return itemsTask.GetAwaiter().GetResult();
         }
 
         public static void WriteTextToFile(IStorageFile file, string text)
         {
             Task writeTask = file.WriteTextAsync(text);
-            writeTask.Wait();
+            writeTask.GetAwaiter().GetResult();
         }
 
         public static IBasicProperties GetBasicProperties(IStorageItem item)
         {
             Task<IBasicProperties> propsTask = item.GetBasicPropertiesAsync();
-            propsTask.Wait();
-            return propsTask.Result;
+            return propsTask.GetAwaiter().GetResult();
         }
 
         public static IDictionary<string, object> RetrieveProperties(IStorageItemProperties item, IEnumerable<string> propertiesToRetrieve)
         {
             Task<IDictionary<string, object>> propsTask = item.Properties.RetrievePropertiesAsync(propertiesToRetrieve);
-            propsTask.Wait();
-            return propsTask.Result;
+            return propsTask.GetAwaiter().GetResult();
         }
 
         public static void DeleteStorageItem(IStorageItem item)
         {
             Task deleteTask = item.DeleteAsync();
-            deleteTask.Wait();
+            deleteTask.GetAwaiter().GetResult();
         }
 
         public static IStorageItem TryGetItem(IStorageFolder folder, string name)
         {
             Task<IStorageItem> getItemTask = folder.TryGetItemAsync(name);
-            getItemTask.Wait();
-            return getItemTask.Result;
+            return getItemTask.GetAwaiter().GetResult();
         }
     }
 }
