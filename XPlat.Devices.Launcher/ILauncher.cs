08191c3449de642c27a633048bdde4fd37c72171
James Croft <jamz_c@hotmail.co.uk>
2018-10-26 07:48:06 +0100

    Updated XML docs for ILauncher.

--- XPlat.Devices.Launcher/ILauncher.cs
+++ XPlat.Devices.Launcher/ILauncher.cs
@@ -12,8 +12,26 @@
         /// <param name="folder">The folder to display in a file explorer.</param>
         Task<bool> LaunchFolderAsync(IStorageFolder folder);
 
+        /// <summary>
+        /// Starts the default app associated with the URI scheme name for the specified URI.
+        /// </summary>
+        /// <param name="uri">
+        /// The URI.
+        /// </param>
+        /// <returns>
+        /// Returns true if the default app for the URI scheme was launched; false otherwise.
+        /// </returns>
         Task<bool> LaunchUriAsync(Uri uri);
 
+        /// <summary>
+        /// Asynchronously query whether an app can be activated for the specified URI.
+        /// </summary>
+        /// <param name="uri">
+        /// The URI for which to query support.
+        /// </param>
+        /// <returns>
+        /// A value that indicates whether an application is available to launch the URI.
+        /// </returns>
         Task<LaunchQuerySupportStatus> QueryUriSupportAsync(Uri uri);
 
         /// <summary>Starts the default app associated with the specified file.</summary>
