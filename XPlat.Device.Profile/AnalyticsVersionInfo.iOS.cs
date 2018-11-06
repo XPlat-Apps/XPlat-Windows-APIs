b080a2de948fe65f7f061583783e9a2d9ec7eb21
James Croft <jamz_c@hotmail.co.uk>
2018-11-06 16:24:45 +0000

    Added analyticsinfo for iOS

---
+++ XPlat.Device.Profile/AnalyticsVersionInfo.iOS.cs
@@ -0,0 +1,45 @@
+﻿#if __IOS__
+namespace XPlat.Device.Profile
+{
+    using UIKit;
+
+    /// <summary>Provides version information about the device family.</summary>
+    public class AnalyticsVersionInfo : IAnalyticsVersionInfo
+    {
+        internal const string DeviceFamilyCar = "Apple.CarPlay";
+
+        internal const string DeviceFamilyDesktop = "Apple.MacOS";
+
+        internal const string DeviceFamilyMobile = "Apple.iPhone";
+
+        internal const string DeviceFamilyTablet = "Apple.iPad";
+
+        internal const string DeviceFamilyTelevision = "Apple.TV";
+
+        internal const string DeviceFamilyUnknown = "Apple.Unknown";
+
+        internal const string DeviceFamilyWatch = "Apple.Watch";
+
+        public string DeviceFamily => DetermineDeviceFamily();
+
+        public string DeviceFamilyVersion => UIKit.UIDevice.CurrentDevice.SystemVersion;
+
+        internal static string DetermineDeviceFamily()
+        {
+            switch (UIKit.UIDevice.CurrentDevice.UserInterfaceIdiom)
+            {
+                case UIUserInterfaceIdiom.Phone:
+                    return DeviceFamilyMobile;
+                case UIUserInterfaceIdiom.Pad:
+                    return DeviceFamilyTablet;
+                case UIUserInterfaceIdiom.TV:
+                    return DeviceFamilyTelevision;
+                case UIUserInterfaceIdiom.CarPlay:
+                    return DeviceFamilyCar;
+                default:
+                    return DeviceFamilyUnknown;
+            }
+        }
+    }
+}
+#endif
