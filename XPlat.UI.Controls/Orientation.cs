a07449ecb38f49abf2633c543aebecc8b6ed4fa7
James Croft <jamz_c@hotmail.co.uk>
2019-03-13 15:22:21 +0000

    Added Controls library, and added Orientation enum

---
+++ XPlat.UI.Controls/Orientation.cs
@@ -0,0 +1,12 @@
+﻿namespace XPlat.UI.Controls
+{
+    /// <summary>Defines constants that specify the different orientations that a control or layout can have.</summary>
+    public enum Orientation
+    {
+        /// <summary>The control or layout should be vertically oriented.</summary>
+        Vertical,
+
+        /// <summary>The control or layout should be horizontally oriented.</summary>
+        Horizontal,
+    }
+}
